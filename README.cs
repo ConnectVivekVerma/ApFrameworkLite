9808f55f5dfde859371aefa1bfe73c45d0132756
ConnectVivekVerma <connectwithvivekverma@gmail.com>
2021-12-10 12:58:15 +0000

    Update README.cs

--- README.cs
+++ README.cs
@@ -1,47 +1,46 @@
 # ApFrameworkLite
 
-private async void HowToUse()
-        {
+private HowToUse()
+{
 
-            /*
-             * Step 1: Get Request Processor   
-             *      A few ways to do this 
-             *          - Extend RequestProcessor class and add required pre and post methods. Then inject instacne to construstor
-             *          - Create RequestProcessorFactory to return RequestProcessor instnace with specific pre/post methods added
-             *          
-             * Step 2:
-             *      Call ProcessRequestAsync method on processor and pass in actual action to perform
-             */
+    /*
+     * Step 1: Get Request Processor   
+     *      A few ways to do this 
+     *          - Extend RequestProcessor class and add required pre and post methods. Then inject instacne to construstor
+     *          - Create RequestProcessorFactory to return RequestProcessor instnace with specific pre/post methods added
+     *          
+     * Step 2:
+     *      Call ProcessRequestAsync method on processor and pass in actual action to perform
+     */
 
-            //get an instance from factory
-            /*
-             * Lets say, i have a function to - 
-             *      get trades from data source (service, DB, etc. ) 
-             *      map to entity and return back object
-             *      on error return status code back             
-             *      
-             *  I will wrap call to this in RequestProcessor
-             */
+    //get an instance from factory
+    /*
+     * Lets say, i have a function to - 
+     *      get trades from data source (service, DB, etc. ) 
+     *      map to entity and return back object                        
+     *      
+     *  I will wrap call to this in RequestProcessor
+     */
 
 
-            RequestActionDelegate getData = async (bag) =>
-            {
-                // Get bag
-                var dataBag = bag as DataBag;
+    RequestActionDelegate getData = async (bag) =>
+    {
+        // Get bag
+        var dataBag = bag as DataBag;
 
-                // Get trades
-                var trades = await DataStore.GetTradesByDate(dataBag.TradeDate);
+        // Get trades
+        var trades = await DataStore.GetTradesByDate(dataBag.TradeDate);
 
-                // Map
-                var tradeJson = await Mapper.Map(trades);
+        // Map
+        var tradeJson = await Mapper.Map(trades);
 
-                // return, by setting in bag. Can also set as local variable within function if object is not needed by any other actions in request processor
-                dataBag.MappedJson = tradeJson;
+        // return, by setting in bag. Can also set as local variable within function if object is not needed by any other actions in request processor
+        dataBag.MappedJson = tradeJson;
 
-            };
+    };
 
-            IRequestProcessor requestProcessor = RequestProcessorFactory.GetRequestProcessor("With_Authorisation_ExceptionLogging_PerfromanceMonitoring");
+    IRequestProcessor requestProcessor = RequestProcessorFactory.GetRequestProcessor("With_Authorisation_ExceptionLogging_PerfromanceMonitoring");
 
-            await requestProcessor.ProcessRequestAsync(getData, false);
+    await requestProcessor.ProcessRequestAsync(getData, false);
 
-        }
+}
